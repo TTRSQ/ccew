a8411dcf7189f043fc9868126cce61cf68226691
TTRSQ <tst.ttrsq.123@gmail.com>
2021-02-21 16:22:43 +0900

    不要な処理の削除

--- src/bitflyer/bitflyer_test.go
+++ src/bitflyer/bitflyer_test.go
@@ -1,39 +1,13 @@
 package bitflyer
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"testing"
 
 	"github.com/TTRSQ/ccew/interface/exchange"
-	"github.com/TTRSQ/ccew/util"
 )
 
 func getExchange() exchange.Exchange {
-	keyFile := "key.json"
-	if util.FileExists(keyFile) {
-		bytes, err := ioutil.ReadFile(keyFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		type key struct {
-			APIKey    string `json:"api_key"`
-			APISecKey string `json:"api_sec_key"`
-		}
-		bfKey := key{}
-		err = json.Unmarshal(bytes, &bfKey)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		bf, _ := New(exchange.Key{
-			APIKey:    bfKey.APIKey,
-			APISecKey: bfKey.APISecKey,
-		})
-		return bf
-	}
 	bf, _ := New(exchange.Key{
 		APIKey:    "hoge",
 		APISecKey: "hoge",
